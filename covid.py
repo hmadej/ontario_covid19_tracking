b1fa9e3b278b0696ca6bb89cd8d71a794865589f
Henry Madej <hmadej@gmail.com>
2020-04-06 10:31:03 -0400

    Updated graphs, including spline line interpolation.

--- covid.py
+++ covid.py
@@ -3,18 +3,19 @@ import json
 import math
 import datetime
 import matplotlib.pyplot as plt
+from scipy.interpolate import UnivariateSpline
+import numpy as np
 from urllib.error import HTTPError
 from urllib.error import URLError
 from urllib.request import urlopen
-
 from bs4 import BeautifulSoup
 
-
 ONTARIO_COVID19_RESOURCE_PAGE = 'https://data.ontario.ca/dataset/status-of-covid-19-cases-in-ontario/resource/ed270bb8' \
-                              '-340b-41f9-a7c6-e8ef587e6d11 '
+                                '-340b-41f9-a7c6-e8ef587e6d11 '
 DOWNLOAD_BUTTON_TAG = 'btn btn-primary dataset-download-link resource-url-analytics resource-type-None'
 TABLE_TAG = 'primary col-sm-9 col-xs-12'
 TABLE_HEADER_STRING = 'Data range end'
+
 
 def get_html(url):
     try:
@@ -37,25 +38,32 @@ def main():
 
     cases = [data[key]['Total Cases'] for key in data.keys()]
     new_cases = [cases[0]]
-    for i in range(0, len(cases)-1):
-        new_cases.append(cases[i+1] - cases[i])
+    for i in range(0, len(cases) - 1):
+        new_cases.append(cases[i + 1] - cases[i])
+
+    plt.style.use('fivethirtyeight')
 
     log_cases = list(map(lambda x: 0 if x <= 0 else math.log(x), cases))
     log_new_cases = list(map(lambda x: 0 if x <= 0 else math.log(x), new_cases))
-    plt.rc('xtick', labelsize=8)
-    plt.plot(log_cases, log_new_cases)
-    plt.ylabel('# of cases')
-    #plt.xticks(cases[-10:], rotation=90)
-    plt.suptitle('loglog Daily case rate')
-    plt.show()
+    spline_curve = UnivariateSpline(log_cases, log_new_cases, s=5)
+    xs = np.linspace(0, max(log_cases), 100)
+    ys = spline_curve(xs)
 
     plt.rc('xtick', labelsize=8)
-    plt.plot(cases, new_cases)
+    plt.plot(log_cases, log_new_cases, '.')
+    plt.plot(xs, ys)
     plt.ylabel('# of cases')
-    #plt.xticks(cases[-10:], rotation=90)
-    plt.suptitle('Daily case rate')
+    # plt.xticks(cases[-10:], rotation=90)
+    plt.suptitle('LogLog Daily Case Rate')
     plt.show()
 
+
+    plt.rc('xtick', labelsize=8)
+    plt.scatter(cases, new_cases)
+    plt.ylabel('# of cases')
+    # plt.xticks(cases[-10:], rotation=90)
+    plt.suptitle('Daily Case Rate')
+    plt.show()
 
 
 def get_data():
@@ -67,7 +75,6 @@ def get_data():
     date, link = None, ''
     current_date = datetime.datetime.now()
     today = f'{current_date.year}-{str(current_date.month).zfill(2)}-{str(current_date.day).zfill(2)}'
-
     with open('datefile.txt', 'r') as f:
         last_update_date = f.read()
 
@@ -106,7 +113,6 @@ def check_when_last_updated():
 
     download_button = bs.find('a', {'class': DOWNLOAD_BUTTON_TAG}, href=True)
     link = download_button['href']
-
     return data_range_end, link
 
 

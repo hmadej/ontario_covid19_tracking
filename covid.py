8a431b5af30a8d3644600ba5831a899e18714a14
Henry Madej <hmadej@gmail.com>
2020-05-05 15:42:13 -0400

    Cleaned up the main script, moved helper functions to fetch.py and refactored helper functions

--- covid.py
+++ covid.py
@@ -1,20 +1,11 @@
-import requests
-import json
-import math
-import datetime
+from json import load
+from math import log
+from datetime import datetime
 import matplotlib.pyplot as plt
-from scipy.interpolate import UnivariateSpline
 import numpy as np
-from urllib.error import HTTPError
-from urllib.error import URLError
-from urllib.request import urlopen
-from bs4 import BeautifulSoup
+from scipy.interpolate import UnivariateSpline
+from fetch import get_date_and_data, save_data_to_file
 
-ONTARIO_COVID19_RESOURCE_PAGE = 'https://data.ontario.ca/dataset/status-of-covid-19-cases-in-ontario/resource/ed270bb8' \
-                                '-340b-41f9-a7c6-e8ef587e6d11 '
-DOWNLOAD_BUTTON_TAG = 'btn btn-primary dataset-download-link resource-url-analytics resource-type-None'
-TABLE_TAG = 'primary col-sm-9 col-xs-12'
-TABLE_HEADER_STRING = 'Data range end'
 THREE_WEEK_DELAY = 21
 
 
@@ -35,27 +26,17 @@ def pull_items(data, key_name):
 
 
 def log_items(data_list):
-    return [0 if x <= 0 else math.log(x) for x in data_list]
-
-
-def get_html(url):
-    '''
-    :param url:
-    :return:
-    '''
-    try:
-        html = urlopen(url)
-    except HTTPError as e:
-        print(e)
-    except URLError as _:
-        print("server could not be found")
-    else:
-        return html
-    return None
+    return [0 if x <= 0 else log(x) for x in data_list]
 
 
 def main():
-    data = get_data()
+    current_date = datetime.now()
+    today = f'{current_date.year}-{str(current_date.month).zfill(2)}-{str(current_date.day).zfill(2)}'
+    date, data = get_date_and_data(today)
+    if date == today:
+        save_data_to_file(date, data)
+
+
     dates = sorted(list(data.keys()))
 
     cumulative_testing = pull_items(data, 'Total patients approved for testing as of Reporting Date')
@@ -77,7 +58,7 @@ def main():
     positive_rate = [abs(a / (1 if b == 0 else b)) * 100 for a, b in zip(new_cases[-30:], new_tests[-30:])]
     plt.plot(dates[-len(positive_rate):][-10:], positive_rate[-10:])
     plt.xticks(dates[-len(positive_rate):][-10:], rotation=90)
-    plt.yticks(np.arange(2, 10, step=0.25))
+    plt.yticks(np.arange(0, max(positive_rate[-10:]), step=0.25))
     plt.suptitle('Positive Case Rate')
     plt.show()
 
@@ -134,7 +115,7 @@ def main():
     plt.show()
 
     with open('conposcovidloc.geojson', 'r') as infile:
-        data = json.load(infile)
+        data = load(infile)
         list_of = data['features']
         count = dict()
         count['unknown'] = 0
@@ -156,75 +137,6 @@ def main():
     plt.bar(dates, date_counts[1])
     plt.xticks(dates, rotation=90)
     plt.show()
-
-
-def get_data():
-    '''
-    if Ontario's website has not updated the data then read from file, otherwise pull
-    the latest data from Ontario's website
-    :return: the data set for covid-19 in ontario as a dict
-    '''
-    date, link = None, ''
-    current_date = datetime.datetime.now()
-    today = f'{current_date.year}-{str(current_date.month).zfill(2)}-{str(current_date.day).zfill(2)}'
-    with open('datefile.txt', 'r') as f:
-        last_update_date = f.read()
-
-    if last_update_date != today:
-        date, link = check_when_last_updated()
-
-    if date and date != last_update_date:
-        print("requesting data")
-        r = requests.get(link)
-        if r.status_code != 200:
-            print(r.status_code)
-            return
-        f = r.text
-        raw_data = f.split('\n')
-        data = process_csv_data(raw_data)
-        with open('data.txt', 'w') as outfile:
-            json.dump(data, outfile)
-        with open('datefile.txt', 'w') as f:
-            f.write(date)
-    else:
-        print(date)
-        with open('data.txt', 'r') as infile:
-            data = json.load(infile)
-
-    return data
-
-
-def check_when_last_updated():
-    '''
-    :return:
-    '''
-    data_range_end = None
-    html = get_html(ONTARIO_COVID19_RESOURCE_PAGE)
-    bs = BeautifulSoup(html.read(), 'html.parser')
-    table = bs.find('div', {'class': TABLE_TAG})
-    for tr in table.find_all("tr"):
-        if tr.th.string == TABLE_HEADER_STRING:
-            data_range_end = tr.td.string.strip()
-            break
-
-    download_button = bs.find('a', {'class': DOWNLOAD_BUTTON_TAG}, href=True)
-    link = download_button['href']
-    return data_range_end, link
-
-
-def process_csv_data(raw_data):
-    '''
-    :param raw_data:
-    :return:
-    '''
-    headers, data = raw_data[0].split(',')[1:], raw_data[1:]
-    headers = list(map(lambda x: x.rstrip(), headers))
-    data_dict = dict()
-    for line in data[:-1]:
-        fields = line.split(',')
-        date, fields = fields[0], list(map(lambda x: (0 if x in {'', '\n', '\r'} else int(x)), fields[1:]))
-        data_dict[date] = dict(zip(headers, fields))
-    return data_dict
 
 
 if __name__ == "__main__":
---
+++ fetch.py
@@ -0,0 +1,91 @@
+import requests
+from datetime import datetime
+from json import dump, load
+from bs4 import BeautifulSoup
+from urllib.error import HTTPError
+from urllib.error import URLError
+from urllib.request import urlopen
+
+
+PATH_TO_DATE_FILE = './datefile'
+PATH_TO_JSON_DATA_FILE = './data.json'
+ONTARIO_COVID19_RESOURCE_PAGE = 'https://data.ontario.ca/dataset/status-of-covid-19-cases-in-ontario/resource' \
+                                '/ed270bb8-340b-41f9-a7c6-e8ef587e6d11 '
+DOWNLOAD_BUTTON_TAG = 'btn btn-primary dataset-download-link resource-url-analytics resource-type-None'
+TABLE_TAG = 'primary col-sm-9 col-xs-12'
+TABLE_HEADER_STRING = 'Data range end'
+
+
+def get_html(url):
+    '''
+    :param url:
+    :return:
+    '''
+    html = None
+    try:
+        html = urlopen(url)
+    except HTTPError as e:
+        print(e)
+    except URLError as u:
+        print(u)
+    finally:
+        return html
+
+
+def text_to_kv_pair(text):
+    csv_key_pair = dict()
+    row_strings = text.split('\n')
+    rows = list(map(lambda x: x.split(','), row_strings))
+    headers, data = rows[0], rows[1:-1]
+    for row in data:
+        date = row[1]
+        csv_key_pair[date] = dict(zip(headers[2:], row[2:]))
+    return csv_key_pair
+
+
+def get_link_to_resource_and_date():
+    '''
+    :return:
+    '''
+    data_range_end = None
+    html = get_html(ONTARIO_COVID19_RESOURCE_PAGE)
+    bs = BeautifulSoup(html.read(), 'html.parser')
+    table = bs.find('div', {'class': TABLE_TAG})
+    for tr in table.find_all("tr"):
+        if tr.th.string == TABLE_HEADER_STRING:
+            data_range_end = tr.td.string.strip()
+            break
+
+    download_button = bs.find('a', {'class': DOWNLOAD_BUTTON_TAG}, href=True)
+    link = download_button['href']
+    return data_range_end, link
+
+
+def save_data_to_file(date, data):
+    with open(PATH_TO_JSON_DATA_FILE, 'w') as outfile:
+        dump(data, outfile)
+    with open(PATH_TO_DATE_FILE, 'w') as f:
+        f.write(date)
+
+
+def get_date_and_data(date):
+    server_date = None
+    with open(PATH_TO_DATE_FILE, 'r') as f:
+        date_from_file = f.read()
+
+    if date_from_file != date:
+        server_date, link = get_link_to_resource_and_date()
+
+    if server_date and server_date != date:
+        print("Requesting new data")
+        if res := requests.get(link) != 200:
+            raise Exception(f"Failed to retrieve date {res.status_code}")
+        else:
+            result = text_to_kv_pair(res.text)
+
+    else:  # load data from file
+        print("Reading data from file, no update")
+        with open(PATH_TO_JSON_DATA_FILE, 'r') as infile:
+            result = load(infile)
+    return date, result
+

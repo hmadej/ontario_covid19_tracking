0d9f2ded423a8b2e10fde321a10cd8d54af580e2
Henry Madej <hmadej@protonmail.com>
2020-07-25 20:24:45 -0400

    refactored weekly average cases to plot against daily count, pulled some helper functions out

--- covid.py
+++ covid.py
@@ -2,6 +2,7 @@ from json import load
 from math import log
 from datetime import datetime
 import matplotlib.pyplot as plt
+from helper import get_regional_data, new_window_average
 from fetch import get_date_and_data, save_data_to_file, text_to_kv_pair, ONTARIO_COVID19_GEOJSON, ONTARIO_COVID19_CSV, \
     ONTARIO_COVID19_POS_LINK, ONTARIO_COVID19_STATUS_LINK
 
@@ -18,7 +19,7 @@ def list_diff(items):
 
 def window_average(data, window):
     reversed_data = list(reversed(data))
-    reversed_data = [sum(reversed_data[i:i + window]) // len(reversed_data[i:i + window]) for i in
+    reversed_data = [sum(reversed_data[i:i + window]) / len(reversed_data[i:i + window]) for i in
                      range(0, len(data), window)]
     return list(reversed(reversed_data))
 
@@ -43,6 +44,10 @@ def main():
     geojson_date, geojson_data = get_date_and_data(today, ONTARIO_COVID19_POS_LINK, ONTARIO_COVID19_GEOJSON)
     if geojson_date == today:
         save_data_to_file(geojson_date, geojson_data, ONTARIO_COVID19_GEOJSON)
+
+    arg = input('Continue? Y/[N] ')
+    if arg[0].lower() != 'y':
+        return 0
 
     print(geojson_data['features'][0]['properties'].keys())
 
@@ -81,6 +86,7 @@ def main():
     plt.show()
 
     def plot_data(plotter, y_data, x_data=None, title=None):
+        y = {}
         for y in y_data:
             plotter.plot(x_data if x_data else range(1, len(y) + 1), y)
         if title:
@@ -93,22 +99,6 @@ def main():
               title='Weekly Death Rate Vs Case Rate Ontario')
     plot_data(plt, [new_deaths[-30:]], x_data=dates[-30:], title='Daily Fatalities, Ontario')
 
-    """
-    log_weekly_cases = log_items(window_average(cumulative_cases, 7))
-    log_new_weekly_cases = log_items(window_average(new_cases, 7))
-
-
-    plt.rc('xtick', labelsize=8)
-    plt.plot(log_weekly_cases, log_new_weekly_cases, '.')
-    plt.ylabel('# of cases')
-    plt.suptitle('LogLog Weekly Case Rate')
-    plt.show()
-
-    plt.bar(dates[-10:], new_tests[-10:])
-    plt.xticks(dates[-10:], rotation=90)
-    plt.suptitle('New Testing Performed')
-    plt.show()
-    """
 
     last_n_days = 14
     moderate = plt.bar(dates[-last_n_days:], new_hospitalizations[-last_n_days:])
@@ -119,98 +109,52 @@ def main():
     plt.legend([moderate, icu, vent], ['Hospital', 'ICU', 'Vent'])
     plt.show()
 
-    days_in_months_2020 = {
-        '01': 31,
-        '02': 29,
-        '03': 31,
-        '04': 30,
-        '05': 31,
-        '06': 30,
-        '07': 31,
-        '08': 31,
-        '09': 30,
-        '10': 31,
-        '11': 30,
-        '12': 31
-    }
+    ontario, cities = get_regional_data(geojson_data)
+    print(ontario)
+    print(cities['Hamilton'])
 
-    def initialize_region_dates(region):
-        for _month, days in days_in_months_2020.items():
-            for _day in range(1, days + 1):
-                region[f'2020-{_month}-{_day:02d}'] = 0
-                if _month == month and day == str(_day).zfill(2):
-                    break
-            if _month == month:
-                break
+    plots = [
+        {
+            'name': 'Hamilton',
+            'data': cities['Hamilton'],
+            'avg_data': new_window_average(cities['Hamilton'], 7)
+        },
+        {
+            'name': 'Oakville',
+            'data': cities['Oakville'],
+            'avg_data': new_window_average(cities['Oakville'], 7)
+        },
+        {
+            'name': 'Windsor',
+            'data': cities['Windsor'],
+            'avg_data': new_window_average(cities['Windsor'], 7)
+        },
+        {
+            'name': 'Sarnia/Lambton',
+            'data': cities['Point Edward'],
+            'avg_data': new_window_average(cities['Point Edward'], 7)
+        },
+        {
+            'name': 'Ontario',
+            'data': ontario,
+            'avg_data': new_window_average(ontario, 7)
+        }
+    ]
 
-    province = dict()
-    initialize_region_dates(province)
+    LAST_N_DAYS = 35 + 1
+    FIRST_N_WEEKS = (LAST_N_DAYS // 7) + 1
+    dates = list(ontario.keys())
 
-    age_groups = ['<20', '20s', '30s', '40s', '50s', '60s', '70s', '80s', '90s', 'UNKNOWN']
+    for place in plots:
+        plt.scatter(list(place['data'].keys())[-LAST_N_DAYS:], list(place['data'].values())[-LAST_N_DAYS:],
+            label=place['name'], alpha=0.7)
+        plt.plot(list(place['avg_data'].keys())[:FIRST_N_WEEKS], list(place['avg_data'].values())[:FIRST_N_WEEKS],
+             label=f'{place["name"]} AVG', alpha=0.7)
 
-    cities = dict()
-    date_key = 'Accurate_Episode_Date'
-    for item in geojson_data['features']:
-        datum = item['properties']
-        if datum['Age_Group'] not in age_groups:
-            continue
-        if (case_date := datum[date_key]) is None:
-            continue
-        if case_date not in province:
-            province[case_date] = 1
-        else:
-            province[case_date] += 1
-
-        if (city := datum['Reporting_PHU_City']) not in cities:
-            cities[city] = dict()
-            initialize_region_dates(cities[city])
-
-        current_city = cities[city]
-        if case_date not in cities[city]:
-            current_city[case_date] = 1
-        else:
-            current_city[case_date] += 1
-
-    print(cities.keys())
-
-    def get_city_count(city_name):
-        return list(zip(*sorted(cities[city_name].items(), key=lambda item: item[0])))
-
-    province_counts = list(zip(*sorted(province.items(), key=lambda item: item[0])))
-    hamilton_counts = get_city_count('Hamilton')
-    oakville_counts = get_city_count('Oakville')
-    windsor_counts = get_city_count('Windsor')
-
-    dates = list(map(lambda x: x[:10], province_counts[0]))
-    hamilton_average_cases = window_average(hamilton_counts[1][-89:-5], 7)
-    oakville_average_cases = window_average(oakville_counts[1][-89:-5], 7)
-    windsor_average_cases = window_average(windsor_counts[1][-89:-5], 7)
-    province_average_cases = window_average(province_counts[1][-89:-5], 7)
-    top_range = max(province_average_cases[-12:])
-    weeks = [x for x in range(1, len(hamilton_average_cases) + 1)]
-    plt.plot(weeks, hamilton_average_cases, label='Hamilton')
-    plt.plot(weeks, oakville_average_cases, label='Halton')
-    plt.plot(weeks, windsor_average_cases, label='Windsor')
-    plt.plot(weeks, province_average_cases, label='Ontario')
-    plt.yticks([i for i in range(0, top_range+5, 5)])
+    plt.xticks(dates[-LAST_N_DAYS:], rotation=90)
     plt.suptitle("Weekly average")
     plt.legend()
     plt.show()
-
-    ont = plt.bar(dates[-42:], province_counts[1][-42:])
-    ham = plt.bar(dates[-42:], hamilton_counts[1][-42:])
-    plt.xticks(dates[-42:], rotation=90)
-    plt.suptitle("Case Count vs Effective Case Date")
-    plt.legend([ham, ont], ['Hamilton', 'Ontario'])
-    plt.show()
-
-    '''
-    other_counts = list(map(list, zip(*sorted(other.items(), key=lambda x: x[0]))))
-    other_dates = list(map(lambda x: x[:10], other_counts[0]))
-    plt.bar(other_dates[-42:], other_counts[1][-42:])
-    plt.xticks(other_dates[-42:], rotation=90)
-    plt.show()
-    '''
 
 
 if __name__ == "__main__":
---
+++ helper.py
@@ -0,0 +1,75 @@
+from datetime import datetime
+
+days_in_months_2020 = {
+    '01': 31,
+    '02': 29,
+    '03': 31,
+    '04': 30,
+    '05': 31,
+    '06': 30,
+    '07': 31,
+    '08': 31,
+    '09': 30,
+    '10': 31,
+    '11': 30,
+    '12': 31
+}
+
+
+def initialize_region_dates(region):
+    current_date = datetime.now()
+    month, day = str(current_date.month).zfill(2), str(current_date.day).zfill(2)
+    for _month, days in days_in_months_2020.items():
+        for _day in range(1, days + 1):
+            region[f'2020-{_month}-{_day:02d}'] = 0
+            if _month == month and day == str(_day).zfill(2):
+                break
+        if _month == month:
+            break
+
+
+def get_regional_data(geojson_data):
+    province, cities = dict(), dict()
+    initialize_region_dates(province)
+    age_groups = ['<20', '20s', '30s', '40s', '50s', '60s', '70s', '80s', '90s', 'UNKNOWN']
+    date_key = 'Accurate_Episode_Date'
+    for item in geojson_data['features']:
+        datum = item['properties']
+        if datum['Age_Group'] not in age_groups:
+            continue
+        if (case_date := datum[date_key]) is None:
+            continue
+
+        case_date = case_date[:10]
+        if case_date not in province:
+            province[case_date] = 1
+        else:
+            province[case_date] += 1
+
+        if (city := datum['Reporting_PHU_City']) not in cities:
+            cities[city] = dict()
+            initialize_region_dates(cities[city])
+
+        current_city = cities[city]
+        if case_date not in cities[city]:
+            current_city[case_date] = 1
+        else:
+            current_city[case_date] += 1
+
+    return province, cities
+
+
+def new_window_average(data, window_length):
+    '''
+    :param data: Dictonary with k,v pair Date, Count
+    :param window_length: length of the window that will be averaged
+    :return: Dictionary with k, v pair Date, average count, date is the last date for the window
+    '''
+    reverse_chronological_order = list(reversed(data.items()))
+    length = len(reverse_chronological_order)
+    average = dict()
+    for i in range(0, length, window_length):
+        window = [item[1] for item in reverse_chronological_order[i:i + window_length]]
+        average[reverse_chronological_order[i][0]] = sum(window) / window_length
+
+    return average
